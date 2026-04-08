e5511c17e8e1ee7eb52bbdfaa52ebc3e778afcb9
Rohan Mia <rohanmia.org@gmail.com>
2026-04-08 17:00:05 +0600

    created books section and book card

---
+++ src/Components/BookCard/BookCard.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { FaRegStar } from "react-icons/fa";
+
+const BookCard = ({ book }) => {
+  return (
+    <div className="card bg-base-100  shadow-sm border border-[#131313]/15 transform  hover:bg-base-200 transition duration-500 hover:scale-105">
+      <figure className="bg-[#f3f3f3] m-6 py-8 px-24 rounded-xl ">
+        <img className="h-[166px]" src={book.image} alt={book.bookName} />
+      </figure>
+      <div className="card-body">
+        <div className="flex items-center gap-3 ">
+          {book.tags.map((tag, idx) => (
+            <div
+              key={idx}
+              className="badge bg-[#23BE0A]/5 text-[#23BE0A] font-semibold rounded-full mb-4 px-4 py-2"
+            >
+              {tag}
+            </div>
+          ))}
+        </div>
+        <h2 className="card-title text-2xl">{book.bookName}</h2>
+        <h3 className="font-semibold text-lg text-[#131313]/80 pb-5 border-b border-dashed border-[#131313]/15">
+          {book.author}
+        </h3>
+        <div className="card-actions flex justify-between items-center mt-5">
+          <div className="text-[#131313]/80 font-semibold text-[16px]">
+            {book.category}
+          </div>
+          <div className="text-[#131313]/80 font-semibold text-[16px] flex items-center gap-2">
+            {book.rating} <FaRegStar />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BookCard;
---
+++ src/Components/homepage/AllBooks.jsx
@@ -0,0 +1,22 @@
+import React, { use } from 'react';
+import BookCard from '../BookCard/BookCard';
+
+const booksPromise = fetch("/booksData.json").then(res=>res.json())
+
+const AllBooks = () => {
+    const books = use(booksPromise)
+    console.log(books);
+    
+    return (
+      <div className="container mx-auto mt-25">
+        <h2 className="text-3xl font-bold flex justify-center mb-10">Books</h2>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+          {books.map((book) => (
+            <BookCard key={book.bookId} book={book}></BookCard>
+          ))}
+        </div>
+      </div>
+    );
+}
+
+export default AllBooks;
